f844fc5dd269b7a7331d79b4396352f1c15321f6
Viswanath <viswanathviswa999@gmail.com>
2026-06-05 10:17:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My First React App</h1>
+      <p>Hello! Welcome to React Development.</p>
+
+      <h2>About Me</h2>
+      <p>Name: Viswanath</p>
+      <p>Course: B.Tech</p>
+
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
